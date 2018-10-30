d7fa587a2f0ab6d0afdf3b5b40aa9bac98a9cb8a
Sheena Mulankova <sheenamulankova@yahoo.com>
2018-10-30 14:11:53 +0700

    Push coba1 dan coba 2

---
+++ resources/views/coba1.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content-header')
+
+<h1>
+	Coba 1
+	<small>Control Panel</small>
+</h1>
+
+<ol class="breadcrumb">
+	<li><a href="/"><i class="fa fa-dashboard"></i>Home</a></li>
+	<li class="active">Coba 1</li>
+</ol>
+
+@endsection
+
+@section('content')
+Halo ini alien 1
+@endsection
